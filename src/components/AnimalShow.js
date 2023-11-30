51750eae225ad1b73ef7d82e033064d84b0bbd37
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2023-11-30 13:33:49 +0100

    Including heart

--- src/components/AnimalShow.js
+++ src/components/AnimalShow.js
@@ -4,13 +4,22 @@ import cow from '../svg/cow.svg'
 import dog from '../svg/dog.svg'
 import gator from '../svg/gator.svg'
 import horse from '../svg/horse.svg'
+import heart from '../svg/heart.svg'
+import { useState } from 'react'
 
 const animalSrcs = { bird, cat, cow, dog, gator, horse };
 
 const AnimalShow = ({ animal }) => {
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+
     return (
-        <div>
+        <div onClick={handleClick}>
             <img alt='animal' src={ animalSrcs[animal] } />
+            <img alt='heart' src={ heart } style={{ width: 10 + 10*clicks + 'px'}}/>
         </div>
     )
 }
